76003c94c47a0b2b32756a6f2664851b1fde947e
Jacqueline Guo <guojacqu@usc.edu>
2026-02-22 16:13:09 -0800

    adding temp wwise events, sounds, etc.

--- proj1-ambiences_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ proj1-ambiences_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,9 +11,22 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMB_BG = 2240465412U;
+        static const AkUniqueID STOP_AMB_BG = 2059548138U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_PLAYERDISTANCE = 3045223865U;
+    } // namespace GAME_PARAMETERS
+
     namespace BUSSES
     {
+        static const AkUniqueID AMB = 1117531639U;
         static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
